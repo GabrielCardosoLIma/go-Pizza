741f6ec70c59cd342593f569dccaf6c2862d4f45
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2023-01-19 19:45:00 -0300

    update 19.01.2023

---
+++ src/Utils/flavorPizzas.ts
@@ -0,0 +1,26 @@
+export const data = [
+    {
+      id: "1",
+      title: "Margherita",
+      description: "Mussarela, manjericão fresco, parmesão e tomate.",
+      image: require("../assets/img/margherita.png"),
+    },
+    {
+      id: "2",
+      title: "4 Queijos",
+      description: "Mussarela, provolone, parmesão e gorgonzola.",
+      image: require("../assets/img/4queijos.png"),
+    },
+    {
+      id: "3",
+      title: "Portuguesa",
+      description: "Calabresa, ovo e pimentão cobertos com mussarela.",
+      image: require("../assets/img/portuguesa.png"),
+    },
+    {
+      id: "4",
+      title: "Lombinho",
+      description: "Mussarela, lombo, requeijão e catupiry.",
+      image: require("../assets/img/lombinho.png"),
+    },
+  ];
